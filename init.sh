75e5779c926120ff876099f9e04d8a752e5c3cb1
Mustafa Murat AYDIN <aydinm.murat@gmail.com>
2020-06-08 13:49:38 +0300

    added configuration files

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+kernelversion=$(uname -r)
+
+modules=$(<files/modules.alias)
+
+cp files/codecs/* /lib/modules/$kernelversion/kernel/sound/soc/codecs/
+cp files/bcm/* /lib/modules/$kernelversion/kernel/sound/soc/bcm/
+cp files/overlays/* /boot/overlays/
+echo "$modules">>/lib/modules/$kernelversion/modules.alias
+
+depmod -a
+
+cp files/config.txt /boot/
+
+dacs=$(<files/dacs.json)
+escapedacs=`echo -e $(printf '%q' $dacs)`
+sed -i -e "s/{\"name\":\"Raspberry PI\",\"data\":\[/{\"name\":\"Raspberry PI\",\"data\":\[${escapedacs}/" /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
+
+cards=$(<files/cards.json)
+escapecards=`echo -e $(printf '%q' $cards)`
+sed -e "s/\"cards\": \[/\"cards\": \[${escapecards}/" /volumio/app/plugins/audio_interface/alsa_controller/cards.json
